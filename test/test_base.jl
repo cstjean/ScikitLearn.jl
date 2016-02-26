b816786161c0df24b9185923d9d1d9cabb15372b
Cedric St-Jean <cstjean@users.noreply.github.com>
2016-02-25 22:44:06 -0500

    More tests

--- test/test_base.jl
+++ test/test_base.jl
@@ -2,8 +2,10 @@ using Base.Test
 using Sklearn
 using Sklearn.GridSearch: GridSearchCV
 using Sklearn.Pipelines: Pipeline
+using PyCall: PyError
 
 @pyimport2 sklearn.svm: SVC
+@pyimport2 sklearn.feature_selection: (SelectFpr, f_classif)
 
 
 function test_is_classifier()
@@ -16,4 +18,35 @@ function test_is_classifier()
 end
 
 
+function test_set_params()
+    # test nested estimator parameter setting
+    clf = Pipeline([("svc", SVC())])
+    # non-existing parameter in svc
+    @test_throws(PyError, set_params!(clf, svc__stupid_param=true))
+    # non-existing parameter of pipeline
+    @test_throws(ArgumentError, set_params!(clf, svm__stupid_param=true))
+end
+
+
+function test_clone()
+    # Tests that clone creates a correct deep copy.
+    # We create an estimator, make a copy of its original state
+    # (which, in this case, is the current state of the estimator),
+    # and check that the obtained copy is a correct deep copy.
+
+    selector = SelectFpr(f_classif, alpha=0.1)
+    new_selector = clone(selector)
+    @test(selector !== new_selector)
+    @test(get_params(selector) == get_params(new_selector))
+
+    selector = SelectFpr(f_classif, alpha=zeros(10, 2))
+    new_selector = clone(selector)
+    @test(selector !== new_selector)
+end
+
+
 test_is_classifier()
+test_set_params()
+test_clone()
+
+:ok
