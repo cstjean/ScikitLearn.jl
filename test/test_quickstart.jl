5d0ef4f570203a7b32576e66d83d776274bd9c26
Cedric St-Jean <cedric.stjean@gmail.com>
2016-06-24 16:18:18 -0400

    Added Quick-Start guide test (make sure it yields the expected results)

---
+++ test/test_quickstart.jl
@@ -0,0 +1,24 @@
+# The quick-start guide code. Some of these are repro tests
+
+using Base.Test
+using RDatasets: dataset
+
+iris = dataset("datasets", "iris")
+
+X = convert(Array, iris[[:SepalLength, :SepalWidth, :PetalLength, :PetalWidth]])
+y = convert(Array, iris[:Species])
+
+using ScikitLearn
+
+@sk_import linear_model: LogisticRegression
+
+model = LogisticRegression(fit_intercept=true, random_state=20)
+
+fit!(model, X, y)
+
+accuracy = sum(predict(model, X) .== y) / length(y)
+@test accuracy == 0.96
+
+using ScikitLearn.CrossValidation: cross_val_score
+
+@test_approx_eq_eps cross_val_score(LogisticRegression(random_state=25), X, y; cv=5) [1.0, 0.96666666667, 0.9333333333, 0.9, 1.0] 0.0001
