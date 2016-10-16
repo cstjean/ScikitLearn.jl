4f53e34dd9152571249be47b2341b7d6bb0b73df
Cedric St-Jean <cedric.stjean@gmail.com>
2016-10-16 17:52:24 -0400

    Improved PolynomialFeatures doc formatting

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -33,20 +33,26 @@ Generate a new feature matrix consisting of all polynomial combinations
 of the features with degree less than or equal to the specified degree.
 For example, if an input sample is two dimensional and of the form
 [a, b], the degree-2 polynomial features are [1, a, b, a^2, ab, b^2].
+
 Parameters
 ----------
-degree : integer
+
+- `degree::Int`
     The degree of the polynomial features. Default = 2.
-interaction_only : boolean, default = False
+- `interaction_only::Bool`, default = False
     If true, only interaction features are produced: features that are
-    products of at most ``degree`` *distinct* input features (so not
-    ``x[1] ** 2``, ``x[0] * x[2] ** 3``, etc.).
-include_bias : boolean
+    products of at most `degree` *distinct* input features (so not
+    `x[1] ^ 2`, `x[0] * x[2] ^ 3`, etc.).
+- `include_bias::Bool`
     If True (default), then include a bias column, the feature in which
     all polynomial powers are zero (i.e. a column of ones - acts as an
     intercept term in a linear model).
+
 Examples
 --------
+
+```julia
+# Python code - TODO: translate
 >>> X = np.arange(6).reshape(3, 2)
 >>> X
 array([[0, 1],
@@ -62,18 +68,23 @@ array([[  1.,   0.,   1.,   0.,   0.,   1.],
 array([[  1.,   0.,   1.,   0.],
        [  1.,   2.,   3.,   6.],
        [  1.,   4.,   5.,  20.]])
+```
+
 Attributes
 ----------
-powers_ : array, shape (n_input_features, n_output_features)
+
+- `powers_::Array`, shape (n_input_features, n_output_features)
     powers_[i, j] is the exponent of the jth input in the ith output.
-n_input_features_ : int
+- `n_input_features_::Int`
     The total number of input features.
-n_output_features_ : int
+- `n_output_features_::Int`
     The total number of polynomial output features. The number of output
     features is computed by iterating over all suitably sized combinations
     of input features.
+
 Notes
 -----
+
 Be aware that the number of features in the output array scales
 polynomially in the number of features of the input array, and
 exponentially in the degree. High degrees can cause overfitting.
