2281cece44bc965fa9f0375187ebc5c42d4ac0e8
Cedric St-Jean <cedric.stjean@gmail.com>
2018-06-11 21:13:00 -0400

    Disable LowRankModels test + use @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,42 +6,63 @@ import PyCall
 PyCall.pyimport_conda("sklearn", "scikit-learn")
 
 using ScikitLearn
+using Base.Test
 
-include("test_models.jl") # runs tests automatically
+@testset "ScikitLearnTests" begin
 
-include("test_base.jl")
-all_test_base()
+    @testset "models" begin
+        include("test_models.jl") # runs tests automatically
+    end
 
-include("test_pipeline.jl")
-all_test_pipeline()
+    @testset "base" begin
+        include("test_base.jl")
+        all_test_base()
+    end
 
-include("test_crossvalidation.jl")
-all_test_crossvalidation()
+    @testset "pipeline" begin
+        include("test_pipeline.jl")
+        all_test_pipeline()
+    end
 
-include("test_utils.jl")
-include("test_quickstart.jl")
-include("test_dataframes.jl")
+    @testset "crossvalidation" begin
+        include("test_crossvalidation.jl")
+        all_test_crossvalidation()
+    end
 
-exceptions = ["Density_Estimation_Julia.ipynb", #GaussianMixtures fails on 0.6 as of MAR17
-              "Plot_Kmeans_Digits_Julia.ipynb", # LowRankModels fails on 0.6 as of MAR17
-              "Simple_1D_Kernel_Density.ipynb", # https://github.com/JuliaPy/PyCall.jl/issues/372
-              ]
-function run_examples()
-    ex_dir = "../examples/"
-    for fname in readdir(ex_dir)
-        if !(fname in exceptions)
-            path = ex_dir * fname
-            if endswith(fname, ".ipynb")
-                println("Testing $path")
-                @eval module Testing
+    @testset "utils" begin
+        include("test_utils.jl")
+    end
+    @testset "quickstart" begin
+        include("test_quickstart.jl")
+    end
+    @testset "DataFrames" begin
+        include("test_dataframes.jl")
+    end
+
+    exceptions = ["Density_Estimation_Julia.ipynb", #GaussianMixtures fails on 0.6 as of MAR17
+                  "Plot_Kmeans_Digits_Julia.ipynb", # LowRankModels fails on 0.6 as of MAR17
+                  "Simple_1D_Kernel_Density.ipynb", # https://github.com/JuliaPy/PyCall.jl/issues/372
+                  ]
+    function run_examples()
+        ex_dir = "../examples/"
+        for fname in readdir(ex_dir)
+            if !(fname in exceptions)
+                path = ex_dir * fname
+                if endswith(fname, ".ipynb")
+                    println("Testing $path")
+                    @eval module Testing
                     using NBInclude
                     nbinclude($path)
+                    end
                 end
             end
         end
     end
-end
 
-run_examples()
+    @testset "Notebook examples" begin
+        run_examples()
+    end
+
+end
 
 nothing
