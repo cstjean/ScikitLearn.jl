bb29a4940d7782725860a85f83b843bde04c7920
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-05-27 18:52:17 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
 # Adapted from scikit-learn
 # Copyright (c) 2007–2016 The scikit-learn developers.
 
-#use non-mkl versions of python packages
-using Conda
-Conda.add("nomkl")
-Conda.rm("mkl")
+#use non-mkl versions of python packages (to allow MAc tests pass)
+@static if Sys.isapple()
+    using Conda
+    Conda.add("nomkl")
+    Conda.rm("mkl")
+end
 
 # Install scikit-learn if not installed
 import PyCall
