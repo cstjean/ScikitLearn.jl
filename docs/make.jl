79cc58c8a1b9cacf505b2dcc61a05b307546be3f
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-05-28 00:19:45 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,11 @@
 using Pkg
 using Documenter
 using ScikitLearn
+import PyCall
+
+
+# Install scikit-learn if not installed
+PyCall.pyimport_conda("sklearn", "scikit-learn")
 
 pages = [
 	"Introduction" => "index.md",
