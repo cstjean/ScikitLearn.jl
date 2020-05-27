f50030c361ebefb314eabde3b9566059231c6ee0
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-05-27 18:05:34 +0100

    check using non-mkl versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 # Adapted from scikit-learn
 # Copyright (c) 2007–2016 The scikit-learn developers.
 
+#use non-mkl versions of python packages
+using Conda
+Conda.add("nomkl")
+Conda.rm("mkl")
+
 # Install scikit-learn if not installed
 import PyCall
 PyCall.pyimport_conda("sklearn", "scikit-learn")
