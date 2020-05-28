ec5a56540efb34c82d13dcf6303745b7f7cd86b3
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-05-28 21:20:05 +0100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,15 @@
 import Conda
 #Workaround the MacOS build issue when building the package with Conda 
-@static if Sys.isapple()
-    if !haskey(ENV, "PYTHON") || ENV["PYTHON"] == "" || lowercase(ENV["PYTHON"]) == "conda"
+
+ if !haskey(ENV, "PYTHON") || ENV["PYTHON"] == "" || ENV["PYTHON"] == "Conda"
+    if Sys.isapple()  
       #use non-mkl versions of python packages
         Conda.add("nomkl")
         Conda.rm("mkl")
+        #installs scikit-learn if not already installed
+        PyCall.pyimport_conda("sklearn", "scikit-learn") 
+    else
+      #installs scikit-learn if not already installed
+        PyCall.pyimport_conda("sklearn", "scikit-learn")
     end
 end 
