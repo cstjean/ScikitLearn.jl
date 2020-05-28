3c7a9ba8a601f5902434a0d0aaf02bc8f6262e51
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2020-05-28 20:24:04 +0100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,7 @@
 import Conda
 #Workaround the MacOS build issue when building the package with Conda 
 @static if Sys.isapple()
-    if !haskey(ENV, "PYTHON") || ENV["PYTHON"] == "" || ENV["PYTHON"] == "Conda"
+    if !haskey(ENV, "PYTHON") || ENV["PYTHON"] == "" || lowercase(ENV["PYTHON"]) == "conda"
       #use non-mkl versions of python packages
         Conda.add("nomkl")
         Conda.rm("mkl")
