f62f5bf9b8540bb9a3a392a88e4ac89b3da2e8be
tylerjthomas9 <tyler@saguarocm.com>
2022-12-20 14:43:34 -0700

    test scikit-learn version patch for julia <=v1.8.3

--- src/Skcore.jl
+++ src/Skcore.jl
@@ -199,18 +199,18 @@ function import_sklearn()
         end
 
     elseif Sys.islinux()
-        libstdcxx_ng_versions = _compatible_libstdcxx_ng_version()
         if !libstdcxx_solved
+            version = _compatible_libstdcxx_ng_version()
             Conda.add("conda", channel="anaconda")
-            Conda.add("libstdcxx-ng$libstdcxx_ng_versions", channel="conda-forge")
+            Conda.add("libstdcxx-ng$version", channel="conda-forge")
+            if version == ">=3.4,<12.0" 
+                # https://github.com/scikit-learn/scikit-learn/pull/23990
+                Conda.add("scikit-learn<1.1", channel="conda-forge") 
+            end
             libstdcxx_solved = true
         end
 
-        if libstdcxx_ng_versions == ">=3.4,<12.0" # https://github.com/scikit-learn/scikit-learn/pull/23990
-            mod = PyCall.pyimport_conda("sklearn", "scikit-learn<=1.0")
-        else
-            mod = PyCall.pyimport_conda("sklearn", "scikit-learn")
-        end
+        mod = PyCall.pyimport_conda("sklearn", "scikit-learn")
     else
         mod = PyCall.pyimport_conda("sklearn", "scikit-learn")
     end
